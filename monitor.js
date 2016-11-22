9166065ca054a99f2fd8473a9ce54dff8f27cd9b
Nic Barker <bicnarker@me.com>
2016-11-22 16:23:05 +1100

    The manager will now search available serial ports for altar workers, connect to spirit via a websocket and listen for updates to worker control values.

--- monitor.js
+++ monitor.js
@@ -1,36 +1,150 @@
-const serialport = require('serialport');
-const SerialPort = serialport.SerialPort;
-const request = require('request-promise');
+const serialport    = require('serialport')
+const SerialPort    = serialport.SerialPort
+const request       = require('request-promise')
+// Connect to the spirit installation and listen for control updates
+const socket        = require('socket.io-client')(process.env.SOCKET_URL)
 
-// Get the device locations for the workers
-if (!process.env.ALTAR_WORKER_DEVICES) {
-    console.log('Error: Please specify the location of altar-worker devices with the ALTAR_WORKER_DEVICES environment variable.');
-    console.log('Hint: here is a list of available serial devices:');
+const altarWorkers = []
+
+// Handle control updates sent from spirit over web socket
+function handleControlUpdates (workerIdentifier, port) {
+    socket.on(`control-update-${workerIdentifier}`, (data) => {
+        console.log(`Processing control value update for worker ${workerIdentifier}`)
+        console.log(data)
+        try {
+            const parsed = JSON.parse(data)
+            if (parsed.controlKey && parsed.controlValue) {
+                port.write(`${parsed.controlKey}:${parsed.controlValue}`, (err) => {
+                    if (err) {
+                        return console.log('Error: ', err.message)
+                    }
+                });
+            } else {
+                console.log('Error: Data over socket did not include a valid controlKey and controlValue')
+            }
+        } catch (error) {
+            console.log(`Error: ${error}`)
+        }
+    })
+}
+
+function listenForMetrics (portName, port) {
+
+    port.on('data', (data) => {
+        try {
+            const parsed = JSON.parse(data)
+            if (parsed.workerIdentifier && parsed.workerIdentifier.length === 36) {
+                request({
+                    method: 'POST',
+                    uri: process.env.SPIRIT_URL + '/metrics',
+                    body: data
+                }).catch((error) => {
+                    console.log(error);
+                });
+            }
+        } catch (error) {
+            console.log(`Parsing Failure: Device ${portName} outputted broken JSON.`)
+            console.log(`Example of data output: ${data}`)
+            port.close()
+        }
+    });
+}
+
+function checkForWorkerIdentifier (portName) {
+    console.log(`Checking if ${portName} has been assigned an identifier...`)
+    const serial = new SerialPort(portName, {
+        parser: serialport.parsers.readline('\r\n'),
+        baudRate: 38400
+    });
+
+    // Time out if no data has been output for 30 seconds
+    const timeout = setTimeout(function () {
+        console.log(`Identification Failure: Device ${portName} isn't sending any output, either isn't an altar-worker or has been disconnected.`)
+        serial.close()
+    }, 30000)
+
+    // Allow up to 5 failures just in case we drop in half way through the serial buffer or something else breaks the output
+    let failureCount = 0
+    serial.on('data', (data) => {
+        try {
+            const parsed = JSON.parse(data)
+            if (parsed.workerIdentifier && parsed.workerIdentifier.length === 36) {
+                console.log(`Identification Success: Device ${portName} has unique identifier ${parsed.workerIdentifier}.`)
+                clearTimeout(timeout)
+                serial.close(function () {
+                    console.log(`Listening / publishing metrics and controls from ${parsed.workerIdentifier}.`)
+                    const serial = new SerialPort(portName, {
+                        parser: serialport.parsers.readline('\r\n'),
+                        baudRate: 38400
+                    });
+                    listenForMetrics(portName, serial)
+                    handleControlUpdates(parsed.workerIdentifier, serial)
+                })
+            } else {
+                failureCount++
+            }
+        } catch (error) {
+            if (failureCount < 5) {
+                failureCount++
+                return
+            }
+            console.log(`Identification Failure: Device ${portName} doesn't appear to be reporting a unique ID. Please run the generate-worker-id utility to write a unique ID to this worker.`)
+            console.log(`Example of data being output: ${data}`)
+            clearTimeout(timeout)
+            serial.close()
+        }
+    });
+}
+
+function identifySerialPorts () {
+    // Get the device locations for the workers
+    console.log('Attempting to locate attached altar worker devices.')
     serialport.list((error, ports) => {
         ports.forEach((port) => {
-            console.log(port.comName);
-        });
-        process.exit(1);
-    });
-} else if (!process.env.SPIRIT_URL) {
-    console.log('Error: Please specify the location of spirit installation with the SPIRIT_URL environment variable.');
-    process.exit(1);
-} else {
-    const workerDevices = process.env.ALTAR_WORKER_DEVICES.split(',');
+            const name = port.comName
+            console.log(`Found ${name}, identifying...`)
 
-    for (workerDevice of workerDevices) {
-        const port = new SerialPort(workerDevice, {
-            parser: serialport.parsers.readline('\r\n'),
-            baudRate: 38400
-        });
-        port.on('data', (data) => {
-            request({
-                method: 'POST',
-                uri: process.env.SPIRIT_URL + '/metrics',
-                body: data
-            }).catch((error) => {
-                console.log(error);
+            const serial = new SerialPort(name, {
+                parser: serialport.parsers.readline('\r\n'),
+                baudRate: 38400
             });
-        });
-    }
+
+            // Time out if no data has been output for 30 seconds
+            const timeout = setTimeout(function () {
+                console.log(`Identification Failure: Device ${name} isn't sending any output, either isn't an altar-worker or has broken output.`)
+                serial.close()
+            }, 30000)
+
+            // Allow up to 5 json failures just in case we drop in half way through the serial buffer or something else breaks the output
+            let failureCount = 0
+            serial.on('data', (data) => {
+                try {
+                    const parsed = JSON.parse(data)
+                    if (parsed.type && parsed.type === 'identification' && parsed.value && parsed.value === 'altar-worker') {
+                        console.log(`Identification Success: Device ${name} is an altar worker.`)
+                        clearTimeout(timeout)
+                        serial.close(function () {
+                            checkForWorkerIdentifier(name)
+                        })
+                    }
+                } catch (error) {
+                    if (failureCount < 5) {
+                        failureCount++
+                        return
+                    }
+                    console.log(`Identification Failure: Device ${name} isn't outputting JSON, either isn't an altar-worker or has broken output.`)
+                    console.log(`Example of data being output: ${data}`)
+                    clearTimeout(timeout)
+                    serial.close()
+                }
+            });
+        })
+    })
+}
+
+if (!process.env.SPIRIT_URL) {
+    console.log('Error: Please specify the location of spirit installation with the SPIRIT_URL environment variable.')
+    process.exit(1)
+} else {
+    identifySerialPorts()
 }
